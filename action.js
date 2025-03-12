b0d005a5d1b6768ca52023eec9ab7342253aafe4
Soham Patil <patilsoham390@gmail.com>
2025-03-12 08:00:36 +0530

    ui logic updated

--- action.js
+++ action.js
@@ -56,26 +56,3 @@ window.addEventListener('scroll', function () {
     fish4.style.left = (value - fish4move) * 1 + 'px';
 })
 
-
-// Contains the link for all social media handles
-var links = document.getElementsByClassName("social-media");
-
-links[0].addEventListener("click", () => { openlink(1) });
-links[1].addEventListener("click", () => { openlink(2) });
-links[2].addEventListener("click", () => { openlink(3) });
-links[3].addEventListener("click", () => { openlink(4) });
-
-function openlink(x) {
-    if (x == 1) {
-        window.open("https://www.instagram.com/_.vini._02_/", "_blank");
-    }
-    if (x == 2) {
-        window.open("https://www.linkedin.com/in/vineet-kumar-gupta-2833ab196/", "_blank");
-    }
-    if (x == 3) {
-        window.open("https://github.com/VineetKumar02", "_blank");
-    }
-    if (x == 4) {
-        window.open("https://vineet-portfolio-site.netlify.app/", "_blank");
-    }
-}
